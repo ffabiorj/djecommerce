89e974ece6c92b6de53e39ec824cc60d8a725a6b
Fabio Oliveira <fabio20rj@gmail.com>
2017-08-12 18:35:18 -0300

    first commit heroku

--- djecommerce/settings.py
+++ djecommerce/settings.py
@@ -120,3 +120,19 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Update database configuration with $DATABASE_URL.
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+
+# Honor the 'X-forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host hedears
+ALLOWED_HOSTS = ['*']
+
+
+# Static files (CSS, Javascript, Images)
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
